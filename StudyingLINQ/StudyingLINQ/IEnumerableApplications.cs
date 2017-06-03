843662ea157502be7f44f5254d038ecd4e1ea5dc
Pedro <pedro.cheroto@gmail.com>
2017-06-03 17:23:55 -0400

    Testing IEnumerable

---
+++ StudyingLINQ/StudyingLINQ/IEnumerableApplications.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyingLINQ
+{
+    class IEnumerableApplications
+    {
+        //Creation of Static Array
+        public IEnumerable<string> nameArray = new string[]{ "Pedro", "Mario", "Guilherme", "Marcos", "Mariana", "Julia" };
+
+        //Creation of Static List
+        public IEnumerable<string> nameList = new List<string>{ "Pedro", "Mario", "Guilherme", "Marcos", "Mariana", "Julia" };
+
+        //Print Array
+        public void PrintArray()
+        {
+            foreach (var name in nameArray)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        //Print List
+        public void PrintList()
+        {
+            foreach (var name in nameList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        //Print IEnumarable
+        public void PrintIEnumerable(IEnumerable<string> names)
+        {
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        public void TestImplementation()
+        {
+            var IA = new IEnumerableApplications();
+            IA.PrintArray();
+            Console.WriteLine("********");
+            IA.PrintList();
+            Console.WriteLine("********");
+            IA.PrintIEnumerable(nameList);
+            Console.WriteLine("********");
+            IA.PrintIEnumerable(nameArray);
+        }
+    }
+}
--- StudyingLINQ/StudyingLINQ/Program.cs
+++ StudyingLINQ/StudyingLINQ/Program.cs
@@ -10,6 +10,7 @@ namespace StudyingLINQ
     {
         static void Main(string[] args)
         {
+            new IEnumerableApplications().TestImplementation();
         }
     }
 }
