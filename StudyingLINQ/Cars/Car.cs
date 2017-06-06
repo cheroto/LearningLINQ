c445569c2c9939bcaf5692bae534c348c2c43441
Pedro <pedro.cheroto@gmail.com>
2017-06-05 22:06:42 -0400

    importing csv and using LINQ on it

---
+++ StudyingLINQ/Cars/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    class Car
+    {
+        public int Year { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Displacement { get; set; }
+        public int NumCylinders { get; set; }
+        public int CityFE { get; set; }
+        public int HighwayFE { get; set; }
+        public int CombFE { get; set; }
+
+
+        internal static Car ParseCarInfo(string data)
+        {
+            var columns = data.Split(',');
+            return new Car
+            {
+                Year = Int32.Parse(columns[0]),
+                Manufacturer = columns[1],
+                Name = columns[2],
+                Displacement = Double.Parse(columns[3]),
+                NumCylinders = Int32.Parse(columns[4]),
+                CityFE = Int32.Parse(columns[5]),
+                HighwayFE = Int32.Parse(columns[6]),
+                CombFE = Int32.Parse(columns[7])
+
+            };
+        }
+    }
+}
---
+++ StudyingLINQ/Cars/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = ReadCarsFromFile("fuel.csv");
+            var query = cars.Where(car => car.Manufacturer == "BMW")
+                            .OrderByDescending(car => car.CityFE)
+                            .ThenBy(car => car.Name)
+                            .Select(car => new
+                                {
+                                    car.Name,
+                                    car.CityFE,
+                                    car.CombFE
+                                }
+                                );
+            foreach (var car in query.Take(20))
+            {
+                Console.WriteLine($"{car.Name}: {car.CityFE}, {car.CombFE}");
+            }
+
+        }
+
+        private static List<Car> ReadCarsFromFile(string path)
+        {
+            return File.ReadAllLines(path)
+                .Skip(1)
+                .Where(line => line.Length > 1)
+                .Select(line => Car.ParseCarInfo(line))
+                .ToList();
+
+        }
+    }
+}
